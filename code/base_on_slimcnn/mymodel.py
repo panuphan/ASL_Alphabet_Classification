830a132b2b3867a439210c7bba293cc8645296eb
Otosi <panuphan1997@gmail.com>
2019-11-30 20:56:10 +0700

    add method img_preprocessing in read_img

--- code/base_on_slimcnn/mymodel.py
+++ code/base_on_slimcnn/mymodel.py
@@ -6,11 +6,13 @@ import numpy as np
 import os, cv2, skimage
 from skimage.transform import resize
 from keras.utils.np_utils import to_categorical
+import imutils
 
 slimcnn_model = 'model/slimcnn/2019-12-01_01.23.00-model'
 AlexNet_model = 'model/myAlexNet/2019-11-30_19.37.12-model'
 VGG16_model = 'model/myVGG16/2019-11-30_23.18.29-model'
 
+#%%
 def reload_model(filename_model_path=AlexNet_model):
     MODEL_SAVE_DIR = filename_model_path + '.h5'
     MODEL_SAVE_WEIGHTS_DIR = filename_model_path + '.weights.h5'
@@ -33,17 +35,41 @@ def reload_model(filename_model_path=AlexNet_model):
         print("Please check if the path is correct or not")
         return None, None
 
+def img_processing(path):
+    
+    # define the upper and lower boundaries of the HSV pixel
+    # intensities to be considered 'skin'
+    lower = np.array([0, 48, 80], dtype = "uint8")
+    upper = np.array([20, 255, 255], dtype = "uint8")
+    
+    # read image from path file
+    img = cv2.imread(path)
+    
+    # resize the frame 
+    frame = imutils.resize(img, width = 400)
+    
+    # convert it to the HSV color space
+    converted = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    
+    #  determine the HSV pixel intensities that fall into
+    #  the speicifed upper and lower boundaries
+    skinMask = cv2.inRange(converted, lower, upper)
+    
+    # blur the mask to help remove noise, then apply the
+    # mask to the frame
+    skinMask = cv2.GaussianBlur(skinMask, (3, 3), 0)
+    skin = cv2.bitwise_and(frame, frame, mask = skinMask)
+    
+    return skin
 
 def read_image(image_path):
     imageSize=64
-    img_file = cv2.imread(image_path)
+    img_file = img_processing(image_path)
     if img_file is not None:
         img_file = skimage.transform.resize(
             img_file, (imageSize, imageSize, 3))
         img_arr = np.asarray(img_file).reshape((-1, imageSize, imageSize, 3))
-
         return img_arr
-
 
 def read_label(prediction):
     CLASSES = ['A',
@@ -74,7 +100,7 @@ def read_label(prediction):
 
 
 #%%
-""" 
+
 #**** EXAMPLE TO USE ****
 model, train_log = reload_model() 
 test_path = 'data/input/pred/B/B1.jpg'
@@ -82,6 +108,6 @@ img = read_image(test_path)
 predictions = model.predict(img)
 print('predictions:', read_label(predictions))
 
- """
+
 
 # %%
